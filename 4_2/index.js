4676f7b3c575fc02b27dabe334ef65cb28e7e2fc
unknown <1176304452@qq.com>
2020-04-02 22:30:17 +0800

    4_3_test

---
+++ 4_2/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-02 10:26:26
+ * @LastEditTime: 2020-04-02 10:38:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test\4_2\index.js
+ */
+
+
+// let a = 10,b = 20, c = 30;
+// ++a;
+// a++;
+// let e = ++a+(b++)+(c++)+a++;
+// console.log(e);
+
+// let arr1 = [1,2,4,5];
+// arr1[5] = 'z';
+// arr2 = arr1.reverse();
+// console.log(`arr1:${arr1}arr2:${arr2}`);
+
+
